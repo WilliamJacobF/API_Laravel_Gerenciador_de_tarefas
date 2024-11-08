7087f7af7f84f6f9dc0114ead905260052496572
WilliamJacobF <155196664+WilliamJacobF@users.noreply.github.com>
2024-11-08 16:55:09 -0300

    Update api.php

--- API_W/routes/api.php
+++ API_W/routes/api.php
@@ -4,16 +4,6 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\TarefaController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 Route::get('/index', [TarefaController::class, 'index'])->name('tarefas-index');
 Route::get('/create', [TarefaController::class, 'create'])->name('tarefas-create');
 Route::post('/', [TarefaController::class, 'store'])->name('tarefas-store');
